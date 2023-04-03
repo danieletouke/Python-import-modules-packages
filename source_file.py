4f96c2557d768240e6ececad4f451c480fffb687
danieletouke <108566435+danieletouke@users.noreply.github.com>
2023-04-03 15:07:48 -0400

    first

---
+++ source_file.py
@@ -0,0 +1,23 @@
+# This file is a python module, the basic file containing functions
+# File Hierrarchy in python:
+# modules > packages&sub-packages(folders/sub-folders containing modules) > libraries&Framework
+# python packages manager is PIP3 like YUM is the redhat packages manager
+
+
+def my_car():
+    return print("camry2020")
+
+def my_next_car():
+    print("venza2023")
+
+def my_next_job():
+    return print("DevOps Engineer")
+
+def input_user():
+    a = input("Enter a word : ")
+    b = str(a)
+    c = len(b)
+    d = f"You entered the word : {b.upper()} it has {c} letters "
+
+    print(d)
+
